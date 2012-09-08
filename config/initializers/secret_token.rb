94b408850902213c95cc648211b05c0d371ab304
Vlad Gurovich <vlad.gurovich@gmail.com>
2012-09-08 15:01:46 -0700

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pitchback::Application.config.secret_token = 'd84b3d3ba824be01088b04fec9d777e59efc3cbd582b5e7c081135e6a5e6ebfea9e9a6da7e3b32e72f1f19cae948e618a71f5af6d30cabd17e242b812f1560c1'
